5b832444e7ce8ef1865eca801c42fbcf51878a93
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-09 02:25:10 +0200

    HelloController - char to clickedButton added

--- src/main/java/org/example/tiktaktoefx/HelloController.java
+++ src/main/java/org/example/tiktaktoefx/HelloController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class HelloController {
 
@@ -13,8 +14,23 @@ public class HelloController {
     @FXML
     private URL location;
 
+    private char nowSym = 'X'; // убрать в класс Constants не получится
+    private char gameField[][] = new char[3][3]; // создание массива для хранения получаемых в процессе игры значений от кнопки
     @FXML
     void btnClick(ActionEvent event) {
+
+        //((Button)event.getSource()).setText(String.valueOf(nowSym)); - быдлокод
+
+        Button clickedButton = (Button) event.getSource();
+
+
+
+
+
+        clickedButton.setText(String.valueOf(nowSym));
+
+        nowSym = nowSym == 'X' ? 'O' : 'X';
+
 
     }
 
