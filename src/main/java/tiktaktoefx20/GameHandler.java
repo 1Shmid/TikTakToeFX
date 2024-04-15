d30fa28fb57a333dfee81043e4ac99ec115dd0d2
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-15 09:55:51 +0200

    GameHandler extracted from ComputerMoveHandler as super class

--- src/main/java/tiktaktoefx20/GameHandler.java
+++ src/main/java/tiktaktoefx20/GameHandler.java
@@ -1,4 +1,114 @@
 package tiktaktoefx20;
 
+import javafx.application.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+import java.util.*;
+
 public class GameHandler {
+    protected final char[][] gameField = new char[Constants.FIELD_SIZE][Constants.FIELD_SIZE]; // создание массива для хранения получаемых в процессе игры значений от кнопки
+    protected String winnerSymbol;
+    @FXML
+    protected GridPane gridPane;
+
+    // Проверка на победителя
+    protected boolean checkForWin() {
+        // Проверка победы по строкам
+        for (int i = 0; i < Constants.FIELD_SIZE; i++) {
+            if (gameField[i][0] == gameField[i][1] &&
+                    gameField[i][0] == gameField[i][2] &&
+                    (gameField[i][0] == 'X' || gameField[i][0] == 'O')) {
+                return true;
+            }
+        }
+
+        // Проверка победы по столбцам
+        for (int i = 0; i < Constants.FIELD_SIZE; i++) {
+            if (gameField[0][i] == gameField[1][i] &&
+                    gameField[0][i] == gameField[2][i] &&
+                    (gameField[0][i] == 'X' || gameField[0][i] == 'O')) {
+                return true;
+            }
+        }
+
+        // Проверка победы по диагоналям
+        return (gameField[0][0] == gameField[1][1] && gameField[0][0] == gameField[2][2] &&
+                (gameField[0][0] == 'X' || gameField[0][0] == 'O')) ||
+                (gameField[0][2] == gameField[1][1] && gameField[0][2] == gameField[2][0]) &&
+                        (gameField[0][2] == 'X' || gameField[0][2] == 'O');
+    }
+
+    // Проверка на ничью
+    protected boolean checkForDraw() {
+        for (int i = 0; i < Constants.FIELD_SIZE; i++) {
+            for (int j = 0; j < Constants.FIELD_SIZE; j++) {
+                if (gameField[i][j] == Constants.EMPTY_SYMBOL) {
+                    // Найдена пустая ячейка, игра не закончилась вничью
+                    return false;
+                }
+            }
+        }
+        return true; // Все ячейки заполнены, ничья
+    }
+
+    @FXML
+    void endGame(Parent root) {
+        // Проверяем условия победы или ничьи
+        String result = "";
+        if (checkForWin()) {
+            result = winnerSymbol + " wins!";
+        } else if (checkForDraw()) {
+            result = "It's a draw!!";
+        }
+
+        // Создаем новое диалоговое окно
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Game result");
+        alert.setHeaderText(null);
+        alert.setContentText(result);
+
+        // Создаем кнопки для новой игры и выхода
+        ButtonType newGameButton = new ButtonType("New Game", ButtonBar.ButtonData.YES);
+        ButtonType exitButton = new ButtonType("Exit", ButtonBar.ButtonData.NO);
+
+        // Устанавливаем кнопки в диалоговом окне
+        alert.getButtonTypes().setAll(newGameButton, exitButton);
+
+
+        // Ожидаем действия пользователя
+        Optional<ButtonType> resultButton = alert.showAndWait();
+
+        // Если пользователь выбрал "Новая игра", начинаем новую игру
+        if (resultButton.isPresent() && resultButton.get() == newGameButton) {
+
+            startNewGame();
+
+        } else {
+            // Иначе закрываем приложение
+            Platform.exit();
+        }
+    }
+
+    protected void startNewGame() {
+
+        // Очищаем игровое поле и включаем все кнопки
+        for (Node node : gridPane.getChildren()) {
+            if (node instanceof Button button) {
+                button.setText("");
+                button.setDisable(false);
+            }
+        }
+
+        // Обнуляем состояние полей массива игры
+        for (int i = 0; i < Constants.FIELD_SIZE; i++) {
+            for (int j = 0; j < Constants.FIELD_SIZE; j++) {
+                gameField[i][j] = Constants.EMPTY_SYMBOL;
+            }
+        }
+    }
 }
