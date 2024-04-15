27aa1a2d2178130e7f6dcb168d4fd6ff7ee1b9d5
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-15 21:44:58 +0200

    GameController - new strategy logic is implemented

--- src/main/java/tiktaktoefx20/GameController.java
+++ src/main/java/tiktaktoefx20/GameController.java
@@ -5,7 +5,7 @@ import javafx.event.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-
+import tiktaktoefx20.strategies.*;
 
 
 public class GameController extends ComputerMoveHandler {
@@ -22,11 +22,51 @@ public class GameController extends ComputerMoveHandler {
     }
 
 
+
+//    @FXML
+//    void btnClick(ActionEvent event) {
+//
+//        Button clickedButton = (Button) event.getSource(); // Получаем кнопку, на которую было нажато
+//
+//        clickedButton.setText(String.valueOf(playerSymbol));
+//        clickedButton.setDisable(true);
+//
+//        // Получаем индексы кнопки
+//        int row = GridPane.getRowIndex(clickedButton) == null ? 0 : GridPane.getRowIndex(clickedButton);
+//        int col = GridPane.getColumnIndex(clickedButton) == null ? 0 : GridPane.getColumnIndex(clickedButton);
+//
+//        // Обновляем состояние игры
+//        gameField[row][col] = playerSymbol;
+//
+//        // Проверяем условия победы или ничьи
+//        if (checkForWin() || checkForDraw()) {
+//            // Если условие победы или ничьи выполнено, игра заканчивается
+//            winnerSymbol = "The player"; // Устанавливаем символ победителя
+//            endGame();
+//        } else {
+//
+//            // Вызываем соответствующий метод для хода компьютера в зависимости от выбранного уровня сложности
+//            String selectedLevel = comb.getSelectionModel().getSelectedItem();
+//            if ("EASY".equals(selectedLevel)) {
+//                computerMoveRandom();
+//            } else if ("HARD".equals(selectedLevel)) {
+//                computerMoveSmart();
+//            } else if ("IMPOSSIBLE".equals(selectedLevel)) {
+//                computerMoveGenius();
+//            }
+//        }
+//    }
+
+
+    private ComputerStrategicMoveHandler computerStrategicMoveHandler;
+
+    char computerSymbol = getComputerSymbol();
+
+    private char[][] gameField = new char[Constants.FIELD_SIZE][Constants.FIELD_SIZE]; // добавляем игровое поле
+
     @FXML
     void btnClick(ActionEvent event) {
-
         Button clickedButton = (Button) event.getSource(); // Получаем кнопку, на которую было нажато
-
         clickedButton.setText(String.valueOf(playerSymbol));
         clickedButton.setDisable(true);
 
@@ -43,25 +83,43 @@ public class GameController extends ComputerMoveHandler {
             winnerSymbol = "The player"; // Устанавливаем символ победителя
             endGame();
         } else {
+            // Получаем символ компьютера
+            char computerSymbol = getComputerSymbol();
 
-            // Вызываем соответствующий метод для хода компьютера в зависимости от выбранного уровня сложности
+            // Вызываем метод makeMove() у экземпляра ComputerStrategicMoveHandler
             String selectedLevel = comb.getSelectionModel().getSelectedItem();
-            if ("EASY".equals(selectedLevel)) {
-                computerMoveRandom();
-            } else if ("HARD".equals(selectedLevel)) {
-                computerMoveSmart();
-            } else if ("IMPOSSIBLE".equals(selectedLevel)) {
-                computerMoveGenius();
+            int[] computerMove = computerStrategicMoveHandler.makeMove(gameField, computerSymbol);
+
+            // Обновляем интерфейс и проверяем условия победы или ничьи
+            int computerRow = computerMove[0];
+            int computerCol = computerMove[1];
+            Button computerButton = getButtonByIndexes(computerRow, computerCol);
+            computerButton.setText(String.valueOf(computerSymbol));
+            computerButton.setDisable(true);
+            gameField[computerRow][computerCol] = computerSymbol;
+
+            if (checkForWin() || checkForDraw()) {
+                winnerSymbol = "The computer"; // Устанавливаем символ победителя
+                endGame();
             }
         }
     }
+
+
+
+//    @FXML
+//    void initialize() {
+//        initializeComboBox();
+//        initializeGameField();
+//    }
+
 
     @FXML
     void initialize() {
         initializeComboBox();
         initializeGameField();
+        computerStrategicMoveHandler = new ComputerStrategicMoveHandler(new RandomMoveStrategy());
     }
-
     private void initializeComboBox() {
 
         ObservableList<String> list = FXCollections.observableArrayList("EASY", "HARD", "IMPOSSIBLE");
