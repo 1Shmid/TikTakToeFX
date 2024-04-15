4f684fe0f6b98ed9bcc7d71ad78f47ee8c8b6c9b
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-15 19:38:03 +0200

    RandomMoveStrategy is created

---
+++ src/main/java/tiktaktoefx20/RandomMoveStrategy.java
@@ -0,0 +1,17 @@
+package tiktaktoefx20;
+
+import java.util.Random;
+
+public class RandomMoveStrategy implements MoveStrategy {
+
+    @Override
+    public int[] makeMove(int[][] gameField, char computerSymbol) {
+        Random random = new Random();
+        int row, col;
+        do {
+            row = random.nextInt(gameField.length);
+            col = random.nextInt(gameField[0].length);
+        } while (gameField[row][col] != Constants.EMPTY_SYMBOL); // Проверяем, что выбранная ячейка свободна
+        return new int[]{row, col};
+    }
+}
