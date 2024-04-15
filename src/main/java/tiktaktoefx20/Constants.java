9fa100c8874d8b557a5534accb2317b977a6aeb8
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-15 21:27:04 +0200

    Constants changed to public

--- src/main/java/tiktaktoefx20/Constants.java
+++ src/main/java/tiktaktoefx20/Constants.java
@@ -1,9 +1,9 @@
 package tiktaktoefx20;
 
-class Constants {
+public class Constants {
     public static final char PLAYER_SYMBOL = 'X';
-    protected static final char X_SYMBOL = 'X';
-    protected static final char O_SYMBOL = 'O';
-    protected static final char EMPTY_SYMBOL = ' ';
-    protected static final int FIELD_SIZE = 3;
+    public static final char X_SYMBOL = 'X';
+    public static final char O_SYMBOL = 'O';
+    public static final char EMPTY_SYMBOL = ' ';
+    public static final int FIELD_SIZE = 3;
 }
