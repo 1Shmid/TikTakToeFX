501dafe2df74147e320e5df20ebbf60318b900d3
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-16 11:08:02 +0200

    GameController - implemented the use of a move strategy

--- src/main/java/tiktaktoefx20/GameController.java
+++ src/main/java/tiktaktoefx20/GameController.java
@@ -75,7 +75,7 @@ public class GameController extends ComputerMoveHandler {
         int row = GridPane.getRowIndex(clickedButton) == null ? 0 : GridPane.getRowIndex(clickedButton);
         int col = GridPane.getColumnIndex(clickedButton) == null ? 0 : GridPane.getColumnIndex(clickedButton);
 
-        // Создаем копию игрового поля
+        // Создаем копию игрового поля - ЗАЧЕМ???
         char[][] tempGameField = new char[Constants.FIELD_SIZE][Constants.FIELD_SIZE];
         for (int i = 0; i < Constants.FIELD_SIZE; i++) {
             System.arraycopy(gameField[i], 0, tempGameField[i], 0, Constants.FIELD_SIZE);
@@ -91,18 +91,67 @@ public class GameController extends ComputerMoveHandler {
             endGame();
         } else {
             // Получаем символ компьютера
-            char computerSymbol = getComputerSymbol();
+            char computerSymbol = getComputerSymbol(); // зачем получать, если он постоянный????
 
             // Вызываем метод makeMove() у экземпляра ComputerStrategicMoveHandler
-            String selectedLevel = comb.getSelectionModel().getSelectedItem();
-            int[] computerMove = computerStrategicMoveHandler.makeMove(tempGameField, computerSymbol);
+//            String selectedLevel = comb.getSelectionModel().getSelectedItem();
+            // int[] computerMove = computerStrategicMoveHandler.makeMove(tempGameField, computerSymbol);
+            /*  здесь нужно передавать текущий уровень сложности selectedLevel для совершения хода, а не вот это вот всё:
+             т.е. другим словами мы здесь говорим: вот тебе текущий уровень сложности, предложи координаты для хода компьютера
+             на выходе получаем координаты
+             следовательно должно быть:
+             int[] computerMove = computerStrategicMoveHandler.makeMove(selectedLevel);
 
-            // Обновляем интерфейс и проверяем условия победы или ничьи
+             ComputerStrategicMoveHandler moveHandler = new ComputerStrategicMoveHandler
+
+
+             В ComputerStrategicMoveHandler
+             public int[] makeMove(selectedLevel) {
+
+
+             */
+
+
+//            ComputerStrategicMoveHandler moveHandler = new ComputerStrategicMoveHandler(new RandomMoveStrategy());
+//
+//            int[] computerMove = moveHandler.makeMove(selectedLevel);
+
+
+            // Создаем объекты стратегий
+            MoveStrategy randomStrategy = new RandomMoveStrategy();
+            MoveStrategy attackStrategy = new AttackMoveStrategy();
+            //MoveStrategy blockOpponentStrategy = new BlockOpponentMoveStrategy();
+            //MoveStrategy centerAndCornersStrategy = new CenterAndCornersMoveStrategy();
+
+            // Создаем объекты обработчиков хода с разными стратегиями
+            ComputerStrategicMoveHandler randomMoveHandler = new ComputerStrategicMoveHandler(randomStrategy);
+            ComputerStrategicMoveHandler attackMoveHandler = new ComputerStrategicMoveHandler(attackStrategy);
+            //ComputerStrategicMoveHandler blockOpponentMoveHandler = new ComputerStrategicMoveHandler(blockOpponentStrategy);
+            //ComputerStrategicMoveHandler centerAndCornersMoveHandler = new ComputerStrategicMoveHandler(centerAndCornersStrategy);
+
+            // Выбираем уровень сложности (стратегию)
+            String selectedLevel = comb.getSelectionModel().getSelectedItem();
+
+            // Делаем ход компьютера с выбранной стратегией
+            int[] computerMove = switch (selectedLevel) {
+                case "EASY" -> randomMoveHandler.makeMove(gameField ,selectedLevel);
+                case "HARD" -> attackMoveHandler.makeMove(gameField ,selectedLevel);
+                //case "BLOCK" -> blockOpponentMoveHandler.makeMove(gameField ,selectedLevel);
+                //case "CENTER_AND_CORNERS" -> centerAndCornersMoveHandler.makeMove(gameField ,selectedLevel);
+                default -> randomMoveHandler.makeMove(gameField ,selectedLevel); // По умолчанию используем случайную стратегию
+            };
+
+            // Выводим ход компьютера
+            System.out.println("Стратегия - " + selectedLevel);
+            System.out.println("Computer move: row = " + computerMove[0] + ", col = " + computerMove[1]);
+
+            // По полученным координатам обновляем графический интерфейс от имени компьютера
             int computerRow = computerMove[0];
             int computerCol = computerMove[1];
             Button computerButton = getButtonByIndexes(computerRow, computerCol);
             computerButton.setText(String.valueOf(computerSymbol));
             computerButton.setDisable(true);
+
             tempGameField[computerRow][computerCol] = computerSymbol;
 
             if (TTTGameLogic.checkForWinS(tempGameField) || TTTGameLogic.checkForDrawS(tempGameField)) {
