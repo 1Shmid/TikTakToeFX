92143189d572bfa16d1dbbc2049b01efbb409e01
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-16 11:06:19 +0200

    Constanta are updated

--- src/main/java/tiktaktoefx20/Constants.java
+++ src/main/java/tiktaktoefx20/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final char O_SYMBOL = 'O';
     public static final char EMPTY_SYMBOL = ' ';
     public static final int FIELD_SIZE = 3;
+    public static final char COMPUTER_SYMBOL = 'O';
 }
