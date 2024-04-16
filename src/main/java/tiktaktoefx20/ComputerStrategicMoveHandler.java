3828365bc12e1af0c3c0d1505a8632b560afc736
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-16 11:05:36 +0200

    ComputerStrategicMoveHandler - Strategy is encapsulated in a specific object

--- src/main/java/tiktaktoefx20/ComputerStrategicMoveHandler.java
+++ src/main/java/tiktaktoefx20/ComputerStrategicMoveHandler.java
@@ -1,29 +1,15 @@
 package tiktaktoefx20;
 
-import tiktaktoefx20.strategies.*;
-
-import java.util.*;
-
 
 public class ComputerStrategicMoveHandler {
-    private MoveStrategy moveStrategy;
+    private final MoveStrategy moveStrategy;
 
     public ComputerStrategicMoveHandler(MoveStrategy moveStrategy) {
         this.moveStrategy = moveStrategy;
     }
-// НЕ ПОНЯТНО, ЗАЧЕМ ДВА РАЗА ВЫЗЫВАТЬ
-    public void setMoveStrategy(MoveStrategy moveStrategy) {
-        this.moveStrategy = moveStrategy;
+
+    public int[] makeMove(char[][] gameField, String selectedLevel) {
+            return moveStrategy.makeMove(gameField, selectedLevel);
     }
 
-    public int[] makeMove(char[][] gameField, char computerSymbol) {
-        Random random = new Random();
-        int row, col;
-        do {
-            row = random.nextInt(gameField.length);
-            col = random.nextInt(gameField[0].length);
-            System.out.println("Selected row: " + row + ", col: " + col);
-        } while (gameField[row][col] != Constants.EMPTY_SYMBOL); // Проверяем, что выбранная ячейка свободна
-        return new int[]{row, col};
-    }
 }
