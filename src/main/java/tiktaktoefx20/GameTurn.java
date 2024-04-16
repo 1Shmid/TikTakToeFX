2a308a707943ded0a60fad72b473ac77c702c9c9
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-17 01:36:18 +0200

    GameTurn is created

---
+++ src/main/java/tiktaktoefx20/GameTurn.java
@@ -0,0 +1,23 @@
+package tiktaktoefx20;
+
+import java.util.Date;
+
+// класс представляет собой запись о ходе в игре, включая номер хода, игрока, координаты хода и временную метку.
+
+public class GameTurn {
+    private int turnNumber;
+    private char player;
+    private int row;
+    private int col;
+    private Date timestamp;
+
+    public GameTurn(int turnNumber, char player, int row, int col, Date timestamp) {
+        this.turnNumber = turnNumber;
+        this.player = player;
+        this.row = row;
+        this.col = col;
+        this.timestamp = timestamp;
+    }
+
+    // Геттеры и сеттеры для всех полей
+}
