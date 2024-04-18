32cf183e17999fff07b0e5b04388e6eac1a4542f
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-18 02:56:29 +0200

    GameMove is created

---
+++ src/main/java/tiktaktoefx20/database/GameMove.java
@@ -0,0 +1,15 @@
+package tiktaktoefx20.database;
+
+public class GameMove {
+    private int moveNumber;
+    private String player;
+    private String coordinates;
+
+    public GameMove(int moveNumber, String player, String coordinates) {
+        this.moveNumber = moveNumber;
+        this.player = player;
+        this.coordinates = coordinates;
+    }
+
+    // Геттеры и сеттеры
+}
