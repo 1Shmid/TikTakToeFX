d9160820100a5f149b553d7f1ba4956724a70f49
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-20 12:04:13 +0200

    GameMove - readGameMoves added

--- src/main/java/tiktaktoefx20/database/GameMove.java
+++ src/main/java/tiktaktoefx20/database/GameMove.java
@@ -1,22 +1,58 @@
 package tiktaktoefx20.database;
 
 import java.sql.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static tiktaktoefx20.database.SQLiteDBManager.DB_URL;
 
 public class GameMove {
     private int moveNumber;
     private String player;
-    private String coordinates;
+    private int row;
+    private int col;
 
-    public GameMove(int moveNumber, String player, String coordinates) {
+    public GameMove(int moveNumber, String player, int row, int col) {
         this.moveNumber = moveNumber;
         this.player = player;
-        this.coordinates = coordinates;
+        this.row = row;
+        this.col = col;
     }
 
+    public static List<GameMove> readGameMoves() {
+        List<GameMove> gameMoves = new ArrayList<>();
 
-    private static final String INSERT_MOVE_SQL = "INSERT INTO moves (game_id, player, row, col) VALUES (?, ?, ?, ?)";
+        // SQL запрос для выборки всех ходов из таблицы game_moves
+        String sql = "SELECT * FROM game_moves";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            // Читаем результаты запроса и создаем объекты GameMove
+            while (rs.next()) {
+                int moveNumber = rs.getInt("move_number");
+                String player = rs.getString("player");
+                int row = rs.getInt("row");
+                int col = rs.getInt("col");
+
+                GameMove gameMove = new GameMove(moveNumber, player, row, col);
+                gameMoves.add(gameMove);
+
+                // Выводим информацию о каждом ходе
+                System.out.println("Move Number: " + moveNumber);
+                System.out.println("Player: " + player);
+                System.out.println("Row: " + row);
+                System.out.println("Column: " + col);
+                System.out.println("-------------------");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return gameMoves;
+    }
 
     // Метод для записи хода игрока или компьютера в базу данных
     public static void recordGameMove(int moveNumber, String player, int row, int col) {
@@ -61,7 +97,6 @@ public class GameMove {
             System.out.println(e.getMessage());
         }
     }
+
     // Геттеры и сеттеры
-
-
 }
