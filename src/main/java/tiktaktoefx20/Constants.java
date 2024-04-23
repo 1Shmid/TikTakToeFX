cb53b135115069df245849fe0f42649e55ae2798
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-24 01:59:36 +0200

    Constants - GAME_TITLE_PREFIX added

--- src/main/java/tiktaktoefx20/Constants.java
+++ src/main/java/tiktaktoefx20/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final char EMPTY_SYMBOL = ' ';
     public static final int FIELD_SIZE = 3;
     public static final char COMPUTER_SYMBOL = 'O';
+    public static final String GAME_TITLE_PREFIX = "TikTacToeFX | Game ";
 }
