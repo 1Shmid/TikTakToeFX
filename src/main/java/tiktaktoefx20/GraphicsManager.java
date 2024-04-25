2a2b91ec4e5ed0e987ea574293d98949ec399774
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-25 03:30:05 +0200

    GraphicsManager is created

---
+++ src/main/java/tiktaktoefx20/GraphicsManager.java
@@ -0,0 +1,13 @@
+package tiktaktoefx20;
+
+import javafx.geometry.*;
+import javafx.scene.shape.Line;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+
+import java.util.*;
+
+public class GraphicsManager extends GameResultHandler {
+
+}
