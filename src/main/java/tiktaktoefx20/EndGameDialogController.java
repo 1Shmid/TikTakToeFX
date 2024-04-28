6e9fff6281d30fea8d7dfb6c6ff5fc0ca37358e2
Juan Copetez Herrero <shmidik@gmail.com>
2024-04-28 15:47:15 +0200

    EndGameDialogController - added different letters color for DRAW result

--- src/main/java/tiktaktoefx20/EndGameDialogController.java
+++ src/main/java/tiktaktoefx20/EndGameDialogController.java
@@ -2,33 +2,73 @@ package tiktaktoefx20;
 
 import javafx.fxml.FXML;
 import javafx.geometry.*;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
 import javafx.stage.*;
 
 public class EndGameDialogController {
 
     @FXML
-    private Label winnerLabel;
+    private Text XText;
+
+    @FXML
+    private Text OText;
 
     @FXML
     private Label resultLabel;
+
+    @FXML
+    private Label winnerLabel;
+
     private Stage stage;
+    public void setWinnerTexts(Text XText, Text OText) {
+        this.XText = XText;
+        this.OText = OText;
+    }
 
     public void initialize() {
-        // Устанавливаем выравнивание текста по центру для обоих Label
-        winnerLabel.setAlignment(Pos.CENTER);
-        resultLabel.setAlignment(Pos.CENTER);
+        // Инициализируем шрифт для текста
+        Font font = new Font("Gill Sans MT", 96);
+
+        // Применяем шрифт к текстовым объектам
+        XText.setFont(font);
+        OText.setFont(font);
     }
 
     public void setWinnerSymbol(String winnerSymbol) {
         if (winnerSymbol.equals("The player")) {
-            winnerLabel.setText("X"); // Если победил игрок, устанавливаем "X" в winnerLabel
+            winnerLabel.setText("X");
+            winnerLabel.setTextFill(Color.BLACK); // Черный цвет для символа "X"
+            winnerLabel.setAlignment(Pos.CENTER);
         } else if (winnerSymbol.equals("The computer")) {
-            winnerLabel.setText("O"); // Если победил компьютер, устанавливаем "O" в winnerLabel
+            winnerLabel.setText("O");
+            winnerLabel.setTextFill(Color.WHITE); // Белый цвет для символа "O"
+            winnerLabel.setAlignment(Pos.CENTER);
         } else {
-            winnerLabel.setText("XO"); // В случае ничьей, устанавливаем "XO" в winnerLabel
+            XText.setText("X");
+            OText.setText("O");
+            XText.setFill(Color.BLACK); // Черный цвет для символа "X"
+            OText.setFill(Color.WHITE); // Белый цвет для символа "O"
+
+            Text xText = new Text(XText.getText());
+            xText.setFill(Color.BLACK);
+            xText.setFont(XText.getFont()); // Используем шрифт из XText
+
+            Text oText = new Text(OText.getText());
+            oText.setFill(Color.WHITE);
+            oText.setFont(OText.getFont()); // Используем шрифт из OText
+
+
+            TextFlow flow = new TextFlow(xText, oText);
+            winnerLabel.setGraphic(flow);
+
+
         }
     }
+
+
 
     public void setResultText(String resultText) {
         if (resultText.contains("wins")) {
@@ -42,6 +82,7 @@ public class EndGameDialogController {
         this.stage = stage;
     }
 
+
     // Метод для расположения окна по центру AnchorPane
     public void centerOnAnchorPane(double anchorPaneWidth, double anchorPaneHeight) {
         double dialogWidth = stage.getWidth();
@@ -53,6 +94,4 @@ public class EndGameDialogController {
         stage.setX(x);
         stage.setY(y);
     }
-
-
 }
