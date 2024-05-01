441756bda4e2f0ec33f0805b974974630295fa3e
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-01 13:43:24 +0200

    module-info.java saved

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.tiktaktoefx {
 
     requires com.dlsc.formsfx;
     requires java.logging;
+    requires java.sql;
 
     opens tiktaktoefx20 to javafx.fxml;
     exports tiktaktoefx20;
