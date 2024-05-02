0285865151bf3d1bbc1867fc9b9a97f216ab7777
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-02 17:33:05 +0200

    MenuActions created. It will be used to store methods that implement menu activities

---
+++ src/main/java/tiktaktoefx20/MenuActions.java
@@ -0,0 +1,14 @@
+package tiktaktoefx20;
+
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+import java.io.*;
+
+public class MenuActions {
+
+
+}
