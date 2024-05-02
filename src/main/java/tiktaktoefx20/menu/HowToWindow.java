bd7fe08e6c1ddbd57954d9f473ef7b0387ccebfa
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-02 18:59:10 +0200

    HowToWindow created

---
+++ src/main/java/tiktaktoefx20/menu/HowToWindow.java
@@ -0,0 +1,56 @@
+package tiktaktoefx20.menu;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+import java.io.*;
+
+public class HowToWindow {
+
+    public static void displayHowToDialog() {
+        String overviewText = getOverviewTextFromReadme("README.md");
+
+        StringBuilder contentTextBuilder = new StringBuilder();
+        contentTextBuilder.append(overviewText.trim()); // Убираем лишние пробелы в начале и конце;
+
+        Alert howDialog = new Alert(Alert.AlertType.INFORMATION);
+        howDialog.setTitle("How to Play");
+        // aboutDialog.setHeaderText("Welcome to Tic-Tac-Toe 2.0!");
+        howDialog.setContentText(contentTextBuilder.toString());
+
+        // Убираем иконку и текст
+        howDialog.setHeaderText(null);
+        howDialog.setGraphic(null);
+
+        howDialog.showAndWait();
+    }
+
+    public static String getOverviewTextFromReadme(String readmeFilePath) {
+        StringBuilder overviewText = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(readmeFilePath))) {
+            boolean inOverviewBlock = false;
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("## How to Play")) {
+                    inOverviewBlock = true;
+                    continue;
+                }
+
+                if (inOverviewBlock) {
+                    if (line.startsWith("##")) {
+                        break; // Покидаем блок Overview при обнаружении следующего заголовка
+                    }
+
+                    overviewText.append(line).append("\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return overviewText.toString();
+    }
+}
+
