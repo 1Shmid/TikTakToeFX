cc59d80ab277f8865c2c3a89b865ed0925914b52
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-02 05:11:49 +0200

    EndGameDialogController - improved code readability and structure

--- src/main/java/tiktaktoefx20/EndGameDialogController.java
+++ src/main/java/tiktaktoefx20/EndGameDialogController.java
@@ -20,48 +20,46 @@ public class EndGameDialogController {
 
     public void setWinnerSymbol(String winnerSymbol) {
         if (winnerSymbol.equals("The player")) {
-            winnerLabel.setText("X");
-            winnerLabel.setTextFill(Color.web("#545454")); // Черный цвет для символа "X"
-            winnerLabel.setAlignment(Pos.CENTER);
+            setSymbolText("X", Color.web("#545454")); // Устанавливаем символ "X" при победе игрока
         } else if (winnerSymbol.equals("The computer")) {
-            winnerLabel.setText("O");
-            winnerLabel.setTextFill(Color.WHITE); // Белый цвет для символа "O"
-            winnerLabel.setAlignment(Pos.CENTER);
+            setSymbolText("O", Color.WHITE); // Устанавливаем символ "O" при победе компьютера
         } else {
-            Text xText = new Text(String.valueOf(Constants.PLAYER_SYMBOL));
-            xText.setFill(Color.web("#545454"));
+            Text xText = createSymbolText(String.valueOf(Constants.PLAYER_SYMBOL), Color.web("#545454"));
             xText.setFont(winnerLabel.getFont());
-
-            Text oText = new Text(String.valueOf(Constants.COMPUTER_SYMBOL));
-            oText.setFill(Color.WHITE);
+            Text oText = createSymbolText(String.valueOf(Constants.COMPUTER_SYMBOL), Color.WHITE);
             oText.setFont(winnerLabel.getFont());
-
             TextFlow flow = new TextFlow(xText, oText);
-            winnerLabel.setGraphic(flow);
+            winnerLabel.setGraphic(flow); // Устанавливаем символ "ХO" при ничьей
+            winnerLabel.setAlignment(Pos.CENTER);
         }
     }
 
+    private void setSymbolText(String symbol, Color color) {
+        winnerLabel.setText(symbol);
+        winnerLabel.setTextFill(color);
+        winnerLabel.setAlignment(Pos.CENTER);
+    }
+
+    private Text createSymbolText(String symbol, Color color) {
+        Text text = new Text(symbol);
+        text.setFill(color);
+        return text;
+    }
+
     public void setResultText(String resultText) {
-        if (resultText.contains("wins")) {
-            resultLabel.setText("WINNER!"); // Устанавливаем "WINNER!" в resultLabel
-        } else {
-            resultLabel.setText("DRAW!"); // Устанавливаем "DRAW!" в resultLabel
-        }
+        resultLabel.setText(resultText.contains("wins") ? "WINNER!" : "DRAW!");
     }
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
-
     // Метод для расположения окна по центру AnchorPane
     public void centerOnAnchorPane(double anchorPaneWidth, double anchorPaneHeight) {
         double dialogWidth = stage.getWidth();
         double dialogHeight = stage.getHeight();
-
         double x = (anchorPaneWidth - dialogWidth) / 2;
         double y = (anchorPaneHeight - dialogHeight) / 2;
-
         stage.setX(x);
         stage.setY(y);
     }
