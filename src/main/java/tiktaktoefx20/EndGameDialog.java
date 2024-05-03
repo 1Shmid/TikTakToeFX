503cdeb31d66097f55ad5ea28e8cc4cb294a4ad9
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-03 03:29:14 +0200

    EndGameDialog - method start relocated to NewGame class

--- src/main/java/tiktaktoefx20/EndGameDialog.java
+++ src/main/java/tiktaktoefx20/EndGameDialog.java
@@ -5,19 +5,17 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Bounds;
 import javafx.scene.*;
-import javafx.scene.canvas.*;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
-import tiktaktoefx20.database.*;
 
 import java.io.IOException;
 
 public class EndGameDialog {
+
+    NewGame newGame = new NewGame();
 
     void show(char[][] gameField, String winnerSymbol, AnchorPane anchorPane, GridPane gridPane, String result) {
         // Загрузите FXML-файл для диалогового окна
@@ -66,7 +64,7 @@ public class EndGameDialog {
         // Устанавливаем обработчик события на клик мышкой
         root.setOnMouseClicked(mouseEvent -> {
             stage.close();
-            startNewGame(gameField, anchorPane, gridPane);
+            newGame.start(gameField, anchorPane, gridPane);
         });
     }
 
@@ -120,66 +118,6 @@ public class EndGameDialog {
         ParallelTransition parallelTransition = new ParallelTransition(scaleTransition, fadeTransition);
 
         return parallelTransition;
-    }
-
-    protected void startNewGame(char[][] gameField, AnchorPane anchorPane, GridPane gridPane) {
-
-        clearCanvas(anchorPane);
-
-        clearGridPaine(gridPane);
-
-        clearGameField(gameField);
-
-        // Обнуляем счетчики ходов
-        GameController.resetMoveCounters();
-
-        // Включаем таймер игры
-        GameController.startGameTimer();
-
-        // Получаем новый номер игры из базы данных
-        int newGameId = SQLiteDBManager.getGameIdFromDatabase();
-
-        // Обновляем заголовок окна с новым номером игры
-        setNewTitleGameNumber(newGameId, gridPane);
-
-    }
-
-    private void setNewTitleGameNumber(int newGameId, GridPane gridPane) {
-
-        String newTitle = Constants.GAME_TITLE_PREFIX + newGameId;
-        Stage stage = (Stage) gridPane.getScene().getWindow();
-        stage.setTitle(newTitle);
-    }
-
-    private static void clearGameField(char[][] gameField) {
-        // Обнуляем состояние полей массива игры
-        for (int i = 0; i < Constants.FIELD_SIZE; i++) {
-            for (int j = 0; j < Constants.FIELD_SIZE; j++) {
-                gameField[i][j] = Constants.EMPTY_SYMBOL;
-            }
-        }
-    }
-
-    private void clearGridPaine(GridPane gridPane) {
-
-        // Очищаем игровое поле и включаем все кнопки
-        for (Node node : gridPane.getChildren()) {
-            if (node instanceof Button button) {
-                button.setText("");
-                button.setDisable(false);
-            }
-        }
-    }
-
-    private static void clearCanvas(AnchorPane anchorPane) {
-        // Проход по всем элементам AnchorPane
-        anchorPane.getChildren().forEach(node -> {
-            // Проверка, является ли текущий элемент Canvas
-            if (node instanceof Canvas) {
-                // Если элемент - Canvas, очистить его
-                ((Canvas) node).getGraphicsContext2D().clearRect(0, 0, ((Canvas) node).getWidth(), ((Canvas) node).getHeight());
-            }
-        });
     }
 
 }
