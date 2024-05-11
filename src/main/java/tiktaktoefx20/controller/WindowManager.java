43bb17d120715fe7d1a90df0adbd3945dad35af1
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-11 02:50:27 +0200

    WindowManager - Interface defining methods for managing application windows created.

---
+++ src/main/java/tiktaktoefx20/controller/WindowManager.java
@@ -0,0 +1,9 @@
+package tiktaktoefx20.controller;
+
+/**
+ * Интерфейс, определяющий методы для работы с окнами приложения. Interface defining methods for
+ * managing application windows.
+ */
+public interface WindowManager {
+
+}
