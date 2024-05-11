ecf39a5d62bad6521e5fa408c7fbe6e3c884e60f
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-11 02:51:20 +0200

    WindowManagerImpl - created. Implementation of the WindowManager interface responsible for managing application windows.

---
+++ src/main/java/tiktaktoefx20/controller/WindowManagerImpl.java
@@ -0,0 +1,10 @@
+package tiktaktoefx20.controller;
+
+/**
+ * Реализация интерфейса WindowManager, отвечающая за управление окнами приложения. Implementation
+ * of the WindowManager interface responsible for managing application windows.
+ */
+
+public class WindowManagerImpl {
+
+}
