23cb2dc1ad8ba28f3a45a4367c17b0819ed22932
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-18 18:09:02 +0200

    module-info.java - new import fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,20 @@
 module org.example.tiktaktoefx {
-  requires javafx.controls;
-  requires javafx.fxml;
-  requires com.dlsc.formsfx;
-  requires java.logging;
-  requires java.sql;
-  requires java.desktop;
-
-  opens tiktaktoefx20 to javafx.fxml;
-  exports tiktaktoefx20;
-  exports tiktaktoefx20.strategies;
-  exports tiktaktoefx20.constants;
-  exports tiktaktoefx20.controller;
-  opens tiktaktoefx20.controller to javafx.fxml;
-  exports tiktaktoefx20.view;
-  opens tiktaktoefx20.view to javafx.fxml;
-  exports tiktaktoefx20.model;
-  opens tiktaktoefx20.model to javafx.fxml;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires com.dlsc.formsfx;
+	requires java.logging;
+	requires java.sql;
+	requires java.desktop;
+	requires org.slf4j;
+	
+	opens tiktaktoefx20 to javafx.fxml;
+	exports tiktaktoefx20;
+	exports tiktaktoefx20.strategies;
+	exports tiktaktoefx20.constants;
+	exports tiktaktoefx20.controller;
+	opens tiktaktoefx20.controller to javafx.fxml;
+	exports tiktaktoefx20.view;
+	opens tiktaktoefx20.view to javafx.fxml;
+	exports tiktaktoefx20.model;
+	opens tiktaktoefx20.model to javafx.fxml;
 }
