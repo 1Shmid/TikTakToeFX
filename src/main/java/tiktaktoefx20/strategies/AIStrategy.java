82dbdb287b3ad771ac7cdd6b9124ec84e23fb658
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-18 15:59:05 +0200

    AIStrategy - a refactored GameParams class has been implemented.

--- src/main/java/tiktaktoefx20/strategies/AIStrategy.java
+++ src/main/java/tiktaktoefx20/strategies/AIStrategy.java
@@ -5,6 +5,7 @@ import tiktaktoefx20.constants.Constants;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import tiktaktoefx20.model.GameParams;
 
 import static tiktaktoefx20.database.SQLiteDBManager.getWinningGameStates;
 
@@ -17,63 +18,88 @@ import static tiktaktoefx20.database.SQLiteDBManager.getWinningGameStates;
  */
 
 public class AIStrategy implements Strategic {
+	
+	@Override
+	public int[] makeMove(GameParams params) {
+		List<char[][]> winningGameStates = getWinningGameStates();
+		List<int[]> possibleMoves = new ArrayList<>();
+		
+		for (char[][] winningGameState : winningGameStates) {
+			for (int row = 0; row < params.getGameField().length; row++) {
+				for (int col = 0; col < params.getGameField()[0].length; col++) {
+					if (params.getGameField()[row][col] == Constants.EMPTY_SYMBOL
+							&& params.getGameField()[row][col] == winningGameState[row][col]) {
+						int matches = countMatches(params.getGameField(), winningGameState, row, col);
+						if (matches > 0) {
+							possibleMoves.add(new int[]{row, col, matches});
+						}
+					}
+				}
+			}
+		}
+		
+		if (!possibleMoves.isEmpty()) {
+			return chooseBestMove(possibleMoves);
+		}
+		
+		return new HardStrategy().makeMove(params);
+	}
 
-  @Override
-  public int[] makeMove(char[][] gameField, String selectedLevel) {
-    List<char[][]> winningGameStates = getWinningGameStates();
-    List<int[]> possibleMoves = new ArrayList<>();
-
-    for (char[][] winningGameState : winningGameStates) {
-      for (int row = 0; row < gameField.length; row++) {
-        for (int col = 0; col < gameField[0].length; col++) {
-          if (gameField[row][col] == Constants.EMPTY_SYMBOL
-              && gameField[row][col] == winningGameState[row][col]) {
-            int matches = countMatches(gameField, winningGameState, row, col);
-            if (matches > 0) {
-              possibleMoves.add(new int[]{row, col, matches});
-            }
-          }
-        }
-      }
-    }
-
-    if (!possibleMoves.isEmpty()) {
-      return chooseBestMove(possibleMoves);
-    }
-
-    return new HardStrategy().makeMove(gameField, selectedLevel);
-  }
-
-  private int countMatches(char[][] currentGameState, char[][] winningGameState, int row, int col) {
-    int matches = 0;
-    for (int i = -1; i <= 1; i++) {
-      for (int j = -1; j <= 1; j++) {
-        if (isValidPosition(row + i, col + j, currentGameState.length, currentGameState[0].length)
-            &&
-            currentGameState[row + i][col + j] == winningGameState[row + i][col + j]) {
-          matches++;
-        }
-      }
-    }
-    return matches;
-  }
-
-  private boolean isValidPosition(int row, int col, int numRows, int numCols) {
-    return row >= 0 && row < numRows && col >= 0 && col < numCols;
-  }
-
-  private int[] chooseBestMove(List<int[]> possibleMoves) {
-    if (possibleMoves.isEmpty()) {
-      return null;
-    }
-
-    // Находим максимальный элемент по третьему элементу (предполагаем, что move[2] содержит приоритет)
-    int[] bestMove = possibleMoves.stream()
-        .max(Comparator.comparingInt(move -> move[2]))
-        .orElse(null);
-
-    // Возвращаем координаты лучшего хода (первые два элемента)
-    return new int[]{bestMove[0], bestMove[1]};
-  }
-
+//	public int[] makeMove(char[][] gameField, String selectedLevel) {
+//		List<char[][]> winningGameStates = getWinningGameStates();
+//		List<int[]> possibleMoves = new ArrayList<>();
+//
+//		for (char[][] winningGameState : winningGameStates) {
+//			for (int row = 0; row < gameField.length; row++) {
+//				for (int col = 0; col < gameField[0].length; col++) {
+//					if (gameField[row][col] == Constants.EMPTY_SYMBOL
+//							&& gameField[row][col] == winningGameState[row][col]) {
+//						int matches = countMatches(gameField, winningGameState, row, col);
+//						if (matches > 0) {
+//							possibleMoves.add(new int[]{row, col, matches});
+//						}
+//					}
+//				}
+//			}
+//		}
+//
+//		if (!possibleMoves.isEmpty()) {
+//			return chooseBestMove(possibleMoves);
+//		}
+//
+//		return new HardStrategy().makeMove(params);
+//	}
+	
+	private int countMatches(char[][] currentGameState, char[][] winningGameState, int row, int col) {
+		int matches = 0;
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (isValidPosition(row + i, col + j, currentGameState.length, currentGameState[0].length)
+						&&
+						currentGameState[row + i][col + j] == winningGameState[row + i][col + j]) {
+					matches++;
+				}
+			}
+		}
+		return matches;
+	}
+	
+	private boolean isValidPosition(int row, int col, int numRows, int numCols) {
+		return row >= 0 && row < numRows && col >= 0 && col < numCols;
+	}
+	
+	private int[] chooseBestMove(List<int[]> possibleMoves) {
+		if (possibleMoves.isEmpty()) {
+			return null;
+		}
+		
+		// Находим максимальный элемент по третьему элементу (предполагаем, что move[2] содержит приоритет)
+		int[] bestMove = possibleMoves.stream()
+				.max(Comparator.comparingInt(move -> move[2]))
+				.orElse(null);
+		
+		// Возвращаем координаты лучшего хода (первые два элемента)
+		return new int[]{bestMove[0], bestMove[1]};
+	}
+	
 }
