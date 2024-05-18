c7aed7e3edd5f7f87d011ecd69cd0960487f6c81
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-18 03:24:14 +0200

    GameParams created from GameEndParams.java. Global variables are aligned

---
+++ src/main/java/tiktaktoefx20/model/GameParams.java
@@ -0,0 +1,33 @@
+package tiktaktoefx20.model;
+
+import java.util.*;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Line;
+import tiktaktoefx20.database.*;
+
+/**
+ * Класс, представляющий набор параметров для окончания игры, необходимых другим классам и методам.
+ * Class representing a set of game ending parameters needed by other classes and methods.
+ */
+
+public record GameParams(String gameWinner,
+												 List<int[]> winningCells,
+												 String difficultyLevel,
+												 List<GameMove> moves,
+												 int moveCounter,
+												 int playerMovesCounter,
+												 int computerMovesCounter,
+												 int gameTime,
+												 AnchorPane anchorPane,
+												 GridPane gridPane,
+												 char[][] gameField,
+												 Line bottomHLine,
+												 Line rightVLine,
+												 Line upHLine,
+												 Line leftVLine
+) {
+
+}
+
