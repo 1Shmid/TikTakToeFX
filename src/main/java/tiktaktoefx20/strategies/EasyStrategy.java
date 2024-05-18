f4a9352af7b3a3d8e90bfc8cef3f8ede0f555aee
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-18 15:59:29 +0200

    EasyStrategy - a refactored GameParams class has been implemented.

--- src/main/java/tiktaktoefx20/strategies/EasyStrategy.java
+++ src/main/java/tiktaktoefx20/strategies/EasyStrategy.java
@@ -3,6 +3,7 @@ package tiktaktoefx20.strategies;
 import tiktaktoefx20.constants.Constants;
 
 import java.util.Random;
+import tiktaktoefx20.model.GameParams;
 
 /**
  * Класс, представляющий реализацию простой стратегии ответного хода. A class representing the
@@ -10,18 +11,30 @@ import java.util.Random;
  */
 
 public class EasyStrategy implements Strategic {
+	
+	@Override
+	public int[] makeMove(GameParams params) {
+		Random random = new Random();
+		int row, col;
+		char[][] gameField = params.getGameField();
+		do {
+			row = random.nextInt(gameField.length);
+			col = random.nextInt(gameField[0].length);
+		} while (gameField[row][col]
+				!= Constants.EMPTY_SYMBOL); // Проверяем, что выбранная ячейка свободна
+		return new int[]{row, col};
+	}
 
-  @Override
-  public int[] makeMove(char[][] gameField, String selectedLevel) {
-    Random random = new Random();
-    int row, col;
-    do {
-      row = random.nextInt(gameField.length);
-      col = random.nextInt(gameField[0].length);
-    } while (gameField[row][col]
-        != Constants.EMPTY_SYMBOL); // Проверяем, что выбранная ячейка свободна
-    return new int[]{row, col};
-  }
+//  public int[] makeMove(char[][] gameField, String selectedLevel) {
+//    Random random = new Random();
+//    int row, col;
+//    do {
+//      row = random.nextInt(gameField.length);
+//      col = random.nextInt(gameField[0].length);
+//    } while (gameField[row][col]
+//        != Constants.EMPTY_SYMBOL); // Проверяем, что выбранная ячейка свободна
+//    return new int[]{row, col};
+//  }
 }
 
 
