25ca08ee5c80ca1291599f4f1a4ec16c13c130c3
Juan Copetez Herrero <shmidik@gmail.com>
2024-05-19 05:27:29 +0200

    GameEngine - code enhancement

--- src/main/java/tiktaktoefx20/model/GameEngine.java
+++ src/main/java/tiktaktoefx20/model/GameEngine.java
@@ -6,7 +6,7 @@ import tiktaktoefx20.controller.Context;
 import tiktaktoefx20.controller.GameController;
 import tiktaktoefx20.controller.GameController.ClickResult;
 import tiktaktoefx20.controller.GameResultHandler;
-import tiktaktoefx20.database.GameMove;
+import tiktaktoefx20.database.GameMoveRecorder;
 import tiktaktoefx20.strategies.AIStrategy;
 import tiktaktoefx20.strategies.EasyStrategy;
 import tiktaktoefx20.strategies.HardStrategy;
@@ -46,44 +46,54 @@ public class GameEngine {
 			return;
 		}
 		
-		// Пришел первый клик и теперь нужно вывести символ.
+		// Выводим символ.
 		gameController.printSymbol(Constants.PLAYER_SYMBOL, clickResult, params);
 		
-		params.getGameField()[clickResult.row()][clickResult.col()] = Constants.PLAYER_SYMBOL;
-		params.setGameField(params.getGameField()); // Обновляем gameField в объекте params
+		// Обновляем поля в объекте GameParams
 		
-		// Увеличиваем счетчик ходов и счетчик ходов игрока
+		params.getGameField()[clickResult.row()][clickResult.col()] = Constants.PLAYER_SYMBOL;
+		params.setGameField(params.getGameField());
+		
+		// Увеличиваем счетчик ходов игрока
 		int playerMovesCounter = params.getPlayerMovesCounter() + 1;
 		params.setPlayerMovesCounter(playerMovesCounter);
 		
-		// Записываем ход игрока
-		GameMove.addMove(moveCounter, "player", clickResult.row(), clickResult.col());
+		// Записываем ход игрока в БД
+		GameMoveRecorder.recordMove(moveCounter, "player", clickResult.row(), clickResult.col());
 		
 		// Обновляем состояние игры
 		updateGameState(params);
 	}
 	
 	private void updateGameState(GameParams params) {
+		
 		if (checkForWinOrDraw(params)) {
 			params.setGameWinner(checkForWin(params) ? "The player" : "It's a draw");
 			params.setWinningCells(GameEngine.winningCells);
 			endGame(params);
 		} else {
+			
 			performComputerMove(params);
+			
 			if (checkForWinOrDraw(params)) {
+				
 				params.setGameWinner(checkForWin(params) ? "The computer" : "It's a draw");
 				params.setWinningCells(GameEngine.winningCells);
 				endGame(params);
+				
 			}
 		}
 	}
 	
 	private void performComputerMove(GameParams params) {
+		
 		int[] computerMove = switch (params.getDifficultyLevel()) {
+			
 			case "EASY" -> easyMoveHandler.makeMove(params);
 			case "HARD" -> hardMoveHandler.makeMove(params);
 			case "AI" -> aiMoveHandler.makeMove(params);
 			default -> easyMoveHandler.makeMove(params);
+			
 		};
 		
 		gameController.printSymbol(Constants.COMPUTER_SYMBOL, computerMove, params);
@@ -95,11 +105,13 @@ public class GameEngine {
 		
 		// Увеличиваем счетчик ходов
 		params.setMoveCounter(params.getMoveCounter() + 1);
+		
 		// Увеличиваем счетчик ходов компьютера
 		params.setComputerMovesCounter(params.getComputerMovesCounter() + 1);
 		
 		// Записываем ход компьютера
-		GameMove.addMove(params.getMoveCounter(), "computer", computerMove[0], computerMove[1]);
+		GameMoveRecorder.recordMove(params.getMoveCounter(), "computer", computerMove[0],
+				computerMove[1]);
 	}
 	
 	private boolean checkForWinOrDraw(GameParams params) {
@@ -122,32 +134,16 @@ public class GameEngine {
 		
 	}
 	
-	private List<GameMove> convertMovesToGameMovesList() {
-		List<GameMove> gameMovesList = new ArrayList<>();
-		for (Object[] move : GameMove.getMoves()) {
+	private List<GameMoveRecorder> convertMovesToGameMovesList() {
+		List<GameMoveRecorder> gameMovesList = new ArrayList<>();
+		for (Object[] move : GameMoveRecorder.getMoves()) {
 			int moveNumber = (int) move[0];
 			String player = (String) move[1];
 			int row = (int) move[2];
 			int col = (int) move[3];
-			gameMovesList.add(new GameMove(moveNumber, player, row, col));
+			gameMovesList.add(new GameMoveRecorder(moveNumber, player, row, col));
 		}
 		return gameMovesList;
-	}
-	
-	
-	// Статическая переменная для хранения координат выигрышных ячеек
-	public static List<int[]> winningCells = new ArrayList<>();
-	
-	// Проверка на победу
-	
-	public static boolean checkForWin(GameParams params) {
-		return checkRowsForWin(params.getGameField()) ||
-				checkColumnsForWin(params.getGameField()) ||
-				checkDiagonalsForWin(params.getGameField());
-	}
-	
-	public static boolean checkForDraw(GameParams params) {
-		return !flattenGameField(params.getGameField()).contains(Constants.EMPTY_SYMBOL);
 	}
 	
 	private static List<Character> flattenGameField(char[][] gameField) {
@@ -161,7 +157,7 @@ public class GameEngine {
 	}
 	
 	// Определение координат выигрышных ячеек в строках
-	static List<int[]> getRowsWinningCellsCoordinates(int row) {
+	private static List<int[]> getRowsWinningCellsCoordinates(int row) {
 		List<int[]> coordinates = new ArrayList<>();
 		for (int j = 0; j < Constants.FIELD_SIZE; j++) {
 			coordinates.add(new int[]{row, j});
@@ -170,7 +166,7 @@ public class GameEngine {
 	}
 	
 	// Определение координат выигрышных ячеек в столбцах
-	static List<int[]> getColumnWinningCellsCoordinates(int col) {
+	private static List<int[]> getColumnWinningCellsCoordinates(int col) {
 		List<int[]> coordinates = new ArrayList<>();
 		for (int i = 0; i < Constants.FIELD_SIZE; i++) {
 			coordinates.add(new int[]{i, col});
@@ -179,7 +175,7 @@ public class GameEngine {
 	}
 	
 	// Определение координат выигрышных ячеек по диагоналям
-	static List<int[]> getDiagonalWinningCellsCoordinates(char[][] gameField) {
+	private static List<int[]> getDiagonalWinningCellsCoordinates(char[][] gameField) {
 		List<int[]> diagonalLeftRight = new ArrayList<>();
 		List<int[]> diagonalRightLeft = new ArrayList<>();
 		for (int i = 0; i < Constants.FIELD_SIZE; i++) {
@@ -192,7 +188,7 @@ public class GameEngine {
 				: diagonalRightLeft;
 	}
 	
-	static boolean checkRowsForWin(char[][] gameField) {
+	private static boolean checkRowsForWin(char[][] gameField) {
 		for (int i = 0; i < Constants.FIELD_SIZE; i++) {
 			if (gameField[i][0] == gameField[i][1] &&
 					gameField[i][0] == gameField[i][2] &&
@@ -206,7 +202,7 @@ public class GameEngine {
 		return false;
 	}
 	
-	static boolean checkColumnsForWin(char[][] gameField) {
+	private static boolean checkColumnsForWin(char[][] gameField) {
 		boolean columnWinFound = false; // Добавляем флаг для отслеживания нахождения выигрыша в столбце
 		for (int i = 0; i < Constants.FIELD_SIZE; i++) {
 			if (gameField[0][i] == gameField[1][i] &&
@@ -221,14 +217,13 @@ public class GameEngine {
 	}
 	
 	// Проверка на победу по диагоналям
-	static boolean checkDiagonalsForWin(char[][] gameField) {
+	private static boolean checkDiagonalsForWin(char[][] gameField) {
 		char symbol = gameField[0][0];
 		
 		if ((symbol == gameField[1][1] && symbol == gameField[2][2] &&
 				(symbol == Constants.X_SYMBOL || symbol == Constants.O_SYMBOL))) {
 			for (int i = 0; i < Constants.FIELD_SIZE; i++) {
 				winningCells.add(new int[]{i, i});
-				
 				winningCells = getDiagonalWinningCellsCoordinates(gameField);
 			}
 			return true;
@@ -238,12 +233,25 @@ public class GameEngine {
 				(gameField[0][2] == Constants.X_SYMBOL || gameField[0][2] == Constants.O_SYMBOL)) {
 			for (int i = 0; i < Constants.FIELD_SIZE; i++) {
 				winningCells.add(new int[]{i, Constants.FIELD_SIZE - 1 - i});
-				
 				winningCells = getDiagonalWinningCellsCoordinates(gameField);
 			}
 			return true;
 		}
-		
 		return false;
+	}
+	
+	// Статическая переменная для хранения координат выигрышных ячеек
+	public static List<int[]> winningCells = new ArrayList<>();
+	
+	// Проверка на победу
+	
+	public static boolean checkForWin(GameParams params) {
+		return checkRowsForWin(params.getGameField()) ||
+				checkColumnsForWin(params.getGameField()) ||
+				checkDiagonalsForWin(params.getGameField());
+	}
+	
+	public static boolean checkForDraw(GameParams params) {
+		return !flattenGameField(params.getGameField()).contains(Constants.EMPTY_SYMBOL);
 	}
 }
